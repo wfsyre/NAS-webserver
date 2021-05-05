3fa0bad63cc0829b8636eedc2ec1bb7cc662c2d9
William Syre <wsyre3@gatech.edu>
2021-05-04 21:08:41 -0400

    moved some buttons to the top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])  if  session[:user_id]
+    @user ||= User.find(session[:user_id])  if  session[:user_id]
   end
 end
